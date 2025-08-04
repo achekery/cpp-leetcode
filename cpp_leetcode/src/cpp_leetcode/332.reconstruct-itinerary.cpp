517b3820459709af9c74a5b8beffa75928f06527
achekery <6503526+achekery@users.noreply.github.com>
2025-08-03 17:34:00 -0700

    332.reconstruct-itinerary

---
+++ cpp_leetcode/src/cpp_leetcode/332.reconstruct-itinerary.cpp
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode id=332 lang=cpp
+ *
+ * [332] Reconstruct Itinerary
+ */
+
+// @lc code=v_start
+
+class Solution {
+public:
+    vector<string> findItinerary(vector<vector<string>>& tickets) {
+        // Find lex min itinerary over V vertices and E edges.
+        // Runtime O(E Log E/V). Memory O(E).
+
+        typedef priority_queue<string, vector<string>, greater<string>>
+            minhp_t;
+        unordered_map<string, minhp_t> adja;
+        deque<string> trav, itin;
+
+        for (auto& t: tickets) {
+            if (adja.find(t[0]) == adja.end()) { adja[t[0]] = minhp_t(); }
+            adja[t[0]].push(t[1]);
+        }
+
+        trav.push_back({"JFK"}); 
+        while (!trav.empty()) {
+            const auto& va = trav.back();
+            if (!adja[va].empty()) {
+                // Discover child node using depth-first search.
+                const auto& vb = adja[va].top();
+                trav.push_back(vb);
+                adja[va].pop();
+            } else {
+                // Explore current node using backtracking.
+                itin.push_front(va);
+                trav.pop_back();
+            }
+        }
+        return vector<string>(itin.begin(), itin.end());
+    }
+};
+
+// @lc code=end
